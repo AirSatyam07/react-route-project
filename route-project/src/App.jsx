25a327edd15e6a92ee11d7ccfa18b11630c5f877
Satyam Kumar <99055360+AirSatyam07@users.noreply.github.com>
2024-12-12 15:03:55 +0000

    first commit route app

---
+++ route-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+  <h1>hey hello</h1>
+   </>
+  )
+}
+
+export default App
